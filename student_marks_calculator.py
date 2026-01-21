c91ea8a9300a88ef3e368210a14dabc657e49894
SudhaMandapati <sudhamandapati90@gmail.com>
2026-01-21 19:36:51 +0530

    Update student_marks_calculator.py

--- student_marks_calculator.py
+++ student_marks_calculator.py
@@ -1,8 +1,8 @@
 name = input("Enter student name: ")
 
-m1 = int(input("Enter marks 1: "))
-m2 = int(input("Enter marks 2: "))
-m3 = int(input("Enter marks 3: "))
+m1 = int(input("Enter marks 1(0-100): "))
+m2 = int(input("Enter marks 2(0-100): "))
+m3 = int(input("Enter marks 3(0-100): "))
 
 total = m1 + m2 + m3
 average = total / 3
